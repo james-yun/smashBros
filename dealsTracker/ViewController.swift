e895d4674a661a1ac623bf97a15264d7e06f126e
James Yun <jy2gm@virginia.edu>
2019-01-10 11:17:55 -0500

    Prints current location into console

--- dealsTracker/ViewController.swift
+++ dealsTracker/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager:CLLocationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        locationManager.startUpdatingLocation()
+        
+        locationManager.distanceFilter = 100
+        
+        let geoFenceRegion:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(43.61871, -116.214607), radius: 100, identifier: "Boise")
+        
+        locationManager.startMonitoring(for: geoFenceRegion)
+        
+        //        locationManager.stopUpdatingLocation()
     }
-
-
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        for currentLocation in locations{
+            print("\(index): \(currentLocation)")
+            // "0: [locations]"
+        }
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entered: \(region.identifier)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Exited: \(region.identifier)")
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
-
